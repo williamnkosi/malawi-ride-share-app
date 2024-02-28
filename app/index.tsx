11533db83372def976ab082e64e08333a2a1f313
William Nkosi <william.dev.acc@gmail.com>
2024-02-28 10:17:24 -0500

    Added dependacies for expo-router

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <View>
+        <Text> HomePage </Text>
+      </View>
+    )
+  }
+}
+
+export default HomePage
