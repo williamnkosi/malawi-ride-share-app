178c8db28b7be7efa4b46b1d77945912bb18325d
William Nkosi <william.dev.acc@gmail.com>
2024-02-28 11:29:43 -0500

    setup bottom tab navigation

---
+++ app/(tabs)/activity/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+type Props = {};
+
+const ActivityPage = (props: Props) => {
+  return (
+    <View>
+      <Text>ActivityPage</Text>
+    </View>
+  );
+};
+
+export default ActivityPage;
